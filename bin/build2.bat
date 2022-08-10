8bb8a26a121e7f7a9c8962d06f7fe3340f50cb92
Krzosa Karol <krzosa.karol@gmail.com>
2022-08-10 13:56:53 +0200

    Building to ship_files

--- bin/build2.bat
+++ bin/build2.bat
@@ -1,15 +1,15 @@
 @echo off
+
 rem todo last flags should be fixed
 set W=-Wno-writable-strings -Wno-deprecated-declarations -Wno-microsoft-goto -Wno-null-dereference -Wno-format
-set L=-luser32.lib -lwinmm.lib -lgdi32.lib -lopengl32.lib -lcomdlg32.lib -luserenv.lib -ldependencies/x64/freetype.lib
-set I=-I"." -I"custom" -I"dependencies/freetype2"
+set L=-luser32.lib -lwinmm.lib -lgdi32.lib -lopengl32.lib -lcomdlg32.lib -luserenv.lib -l..\dependencies/x64/freetype.lib
+set I=-I".." -I"..\custom" -I"..\dependencies/freetype2"
 set N=-o 4ed
 set F=-g
 
+pushd %~dp0\..\ship_files
 call custom/bin/buildsuper_x64-win.bat
-clang 4ed_app_target.cpp %W% %I% %F% -shared %L% %N%_app.dll
-clang platform_win32/win32_4ed.cpp %W% %I% %F% %L% %N%.exe
+clang ..\4ed_app_target.cpp %W% %I% %F% -shared %L% %N%_app.dll
+clang ..\platform_win32/win32_4ed.cpp %W% %I% %F% %L% %N%.exe
+popd
 
-copy *.exe ship_files
-copy *.pdb ship_files
-copy *.dll ship_files
