99a4f57b6e5ec9a89232efcead2832600f4d3b01
Krzosa Karol <krzosa.karol@gmail.com>
2022-08-12 13:21:22 +0200

    Showing whitespace in comments and literals

--- custom/4coder_code_index.cpp
+++ custom/4coder_code_index.cpp
@@ -1011,7 +1011,13 @@ layout_index__inner(Application_Links *app, Arena *arena, Buffer_ID buffer, Rang
                     lr_tb_advance_x_without_item(&pos_vars, shift);
                     goto consuming_normal_whitespace;
                 }
-                // else if(*ptr == ' '){
+                else if(*ptr == ' '){
+                    i64 index = layout_index_from_ptr(ptr, text.str, range.first);
+                    Token_Iterator_Array it = token_iterator_pos(0, &tokens, index);
+                    Token *b = token_it_read(&it);
+                    if(b->kind == TokenBaseKind_LiteralString || b->kind == TokenBaseKind_Comment){
+                        lr_tb_advance_x_without_item(&pos_vars, metrics.space_advance);
+                    }
                 //     i64 index = layout_index_from_ptr(ptr, text.str, range.first);
                 //     Token_Pair pair = layout_token_pair(tokens_ptr, index);
                 //     if(pair.a.kind == TokenBaseKind_LiteralString || pair.b.kind == TokenBaseKind_LiteralString){
@@ -1019,7 +1025,7 @@ layout_index__inner(Application_Links *app, Arena *arena, Buffer_ID buffer, Rang
                 //         f32 shift = layout_index_x_shift(app, &reflex, file, index, regular_indent);
                 //         lr_tb_advance_x_without_item(&pos_vars, shift);
                 //     }
-                // }
+                }
             }
 
             if (ptr == end_ptr){
